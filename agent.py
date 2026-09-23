6c50e60913f399108e4a729ba5b90a38200fdc96
Samir-BK <bhu041@myy.haaga-helia.fi>
2026-09-23 11:15:15 +0300

    Add build agent function

--- agent.py
+++ agent.py
@@ -1,4 +1,9 @@
 from agno.agent import Agent # Agent
-
 from agno.models.groq import Groq # Model
 
+def build_agent():
+    return Agent(
+        model=Groq(id="openai/gpt-oss-120b"),
+        markdown = True,
+        instructions = "You are a helpful and expert travel agent"
+    )
