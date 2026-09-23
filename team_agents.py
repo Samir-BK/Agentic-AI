428a95fe469de1f5862f3f8311d7488d78687f14
Samir-BK <bhu041@myy.haaga-helia.fi>
2026-09-23 13:00:42 +0300

    Add team_agents and imports

---
+++ team_agents.py
@@ -0,0 +1,11 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.team.mode import TeamMode
+from agno.team.team import Team
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+## Creating Members
