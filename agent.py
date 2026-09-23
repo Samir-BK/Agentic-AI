872985ae5fc73ab4ff61a2aebb0a9c461f6c6194
Samir-BK <bhu041@myy.haaga-helia.fi>
2026-09-23 11:28:29 +0300

    Testing agent with simple queries

--- agent.py
+++ agent.py
@@ -1,9 +1,16 @@
 from agno.agent import Agent # Agent
 from agno.models.groq import Groq # Model
 
+from dotenv import load_dotenv
+load_dotenv()
+
 def build_agent():
     return Agent(
         model=Groq(id="openai/gpt-oss-120b"),
         markdown = True,
-        instructions = "You are a helpful and expert travel agent"
+        instructions = "You are a helpful and expert travel agent."
     )
+
+groq_agent = build_agent()
+
+groq_agent.print_response("What is your knowöedge cutoff?")
